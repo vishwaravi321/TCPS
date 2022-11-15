2908d13c4ae69bac4d547e0869ea3dc4a3eb7bad
vishwa <vishwaravi321@gmail.com>
2022-11-15 13:15:15 +0530

    sudden motive

---
+++ Java/return.java
@@ -0,0 +1,13 @@
+class fun
+{
+    public static void main(String asd[])
+    {
+        int c,b;
+        int boo(int a,int b)
+        {
+          c=a+b;
+          return c;  
+        }
+        b=boo(4,5);
+    }
+}
